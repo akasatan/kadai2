065633e07480479306ce2838f981005a5bf70845
EC2 Default User <aka435304@gmail.com>
2021-02-04 03:08:18 +0000

    [finsh]2-7-2

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -1,23 +1,24 @@
 puts "計算を始めます"
 puts "何回計算を繰り返しますか？"
- kaisu = gets
-for i in 1..kaisu.to_i do
-  #iを入力された分だけ繰り返す
-  if i > kaisu.to_i
-    puts "計算を終了します"
-    break
-    #終わったら終了する
-  else
+
+ kaisu = gets.to_i
+i = 1
+while i <= kaisu do
     puts "#{i}回目の計算"
     puts "2つの値を入力してください"
-     akey = gets
-     bkey = gets
-    puts "a=#{akey.to_i}"
-    puts "b=#{bkey.to_i}"
+    
+     akey = gets.to_i
+     bkey = gets.to_i
+     
+    puts "a=#{akey}"
+    puts "b=#{bkey}"
+    
     puts "計算結果を出力します"
-    puts "a+b=#{akey.to_i + bkey.to_i}"
-    puts "a-b=#{akey.to_i - bkey.to_i}"
-    puts "a*b=#{akey.to_i * bkey.to_i}"
-    puts "a/b=#{akey.to_i / bkey.to_i}"
-  end
+    puts "a+b=#{akey + bkey}"
+    puts "a-b=#{akey - bkey}"
+    puts "a*b=#{akey * bkey}"
+    puts "a/b=#{akey / bkey}"
+    
+    i += 1
 end
+    puts "計算を終了します"
